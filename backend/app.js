12dfa757e33fb9dc7a82dbfcc77549983fad8a15
nikhil <niikhiil077@gmail.com>
2026-06-22 13:53:31 +0530

    App updated

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { router } from "./app.route.js";
-import dotenv from "dotenv";
+
 
 export const app = express();
 
 app.use(express.json())
-dotenv.config();
 app.use(router);
